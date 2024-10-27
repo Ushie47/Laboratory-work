5cbabb197204c1de5b4ba5ab93dfb6f469bc63c2
David Michael Francis <omotole47@gmail.com>
2024-10-27 20:01:15 +0300

    Add lab4.asm

---
+++ Архитектура компьютера/arch-pc/labs/lab04/lab4.asm
@@ -0,0 +1,17 @@
+SECTION .data
+    hello: db 'David, Michael Francis!', 10   ; Define a string with a newline
+    helloLen equ $ - hello          ; Calculate the length of the string
+
+SECTION .text
+    global _start
+
+_start:
+    mov eax, 4          ; sys_write (4)
+    mov ebx, 1          ; file descriptor (stdout)
+    mov ecx, hello      ; pointer to string
+    mov edx, helloLen   ; length of the string
+    int 0x80            ; interrupt to invoke system call
+
+    mov eax, 1          ; sys_exit (1)
+    xor ebx, ebx        ; return 0 status
+    int 0x80            ; interrupt to invoke system call
